c2de131e122a4a9776fb7c0254aad9f187af947c
Zlatomir Kosev <zlatomir_hk@hotmail.com>
2020-09-10 14:23:36 +0100

    Initial Commit react app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBzjo0e-4gpobBKGLPLBnQ-3yipsxEpTGg",
+  authDomain: "clone-a1bca.firebaseapp.com",
+  databaseURL: "https://clone-a1bca.firebaseio.com",
+  projectId: "clone-a1bca",
+  storageBucket: "clone-a1bca.appspot.com",
+  messagingSenderId: "34496862022",
+  appId: "1:34496862022:web:cb144c4ef5c8c709673747",
+  measurementId: "G-Y2QT5PZBBN",
+};
